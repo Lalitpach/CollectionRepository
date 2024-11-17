cc69d74d9dcfd6d66fe207540db0b16ff7b33bfe
Lalitpach <lalitpachpande111@gmail.com>
2024-11-17 11:20:13 +0800

    Add project files.

---
+++ CollectionRepository/Collection queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionRepository
+{
+    internal class Collection_queue
+    {
+    }
+}
